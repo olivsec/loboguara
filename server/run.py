53729ac3f5003621dd80ec168ea018d863eafb34
Jonathan Machado <jhonatha.oliv@gmail.com>
2024-11-07 20:37:17 -0300

    Update run.py
    Inclusion of message about the start of application execution.

--- server/run.py
+++ server/run.py
@@ -5,12 +5,14 @@ import subprocess
 app = create_app()
 
 if __name__ == "__main__":
-    
-    host = os.getenv('LOBOGUARA_HOST', '0.0.0.0')  
-    port = int(os.getenv('LOBOGUARA_PORT', 7405))   
 
-    
+    host = os.getenv('LOBOGUARA_HOST', '0.0.0.0')
+    port = int(os.getenv('LOBOGUARA_PORT', 7405))
+
+    # Iniciar o agendador
     start_scheduler(app)
+
+    # Iniciar o gRPC Client como processo separado
     grpc_client_path = os.path.join(os.path.dirname(__file__), 'gRPC_Client.py')
     try:
         subprocess.Popen(['python3', grpc_client_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -18,5 +20,9 @@ if __name__ == "__main__":
     except Exception as e:
         app.logger.error(f"Erro ao iniciar o gRPC Client: {str(e)}")
 
-    
+    # Exibir mensagem de inicialização
+    print(f"Lobo Guará has started successfully! The application is ready to use.")
+    print(f"Access the application at: http://{host}:{port}")
+
+    # Executar a aplicação
     socketio.run(app, host=host, port=port, debug=False)
